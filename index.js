2c41ba16fd23fae0ea56e3fb03688e2572280672
MiguelLuceroSL <roquerobertomiguellucero@gmail.com>
2024-04-29 19:57:41 -0300

    get inicio

--- index.js
+++ index.js
@@ -16,8 +16,45 @@ function generarID() {
     return Math.random().toString(36).substr(2, 9);
 }
 
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
+app.get('/', async (req, res) => {
+    const { category } = req.query;
+    let url = 'https://fakestoreapi.com/products';
+    try {
+        if (category) {
+            url += `/category${category}`;
+        }
+        const response = await fetch(url);
+        let productos = await response.json();
+        productos = await Promise.all(productos.map(async (producto) => {
+            let titulo = await traducir(producto.title);
+            let descripcion = await traducir(producto.description);
+            let categoria = await traducir(producto.category);
+            titulo = primeraLetra(titulo);
+            descripcion = primeraLetra(descripcion);
+            categoria = primeraLetra(categoria);
+            producto.title = titulo;
+            producto.description = descripcion;
+            producto.category = categoria;
+            const descuento = descuentos.find(descuento => descuento.id === producto.id);
+            if (descuento) {
+                producto.descuento = descuento.descuento;
+                let descuentoValor = producto.price * (producto.descuento / 100);
+                let descuentoValorDecimal = parseFloat(descuentoValor).toFixed(2);
+                producto.descuentoEnDinero = Number(descuentoValorDecimal);
+                let precioFinal = producto.price - producto.descuentoEnDinero;
+                let precioFinalDecimal = parseFloat(precioFinal).toFixed(2);
+                producto.precioConDescuentoAplicado = Number(precioFinalDecimal);
+                producto.tieneDescuento = true;
+            } else {
+                producto.tieneDescuento = false
+            }
+            return producto;
+        }));
+        res.json(productos);
+    } catch (error) {
+        console.error('Error al obtener productos: ', error);
+        res.status(500).json({ error: 'Error al obtener productos' });
+    }
 });
 
 app.post('/comprar', (req, res) => {
